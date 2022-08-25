416bc38667ef497950fbb3b4fbd92770759809df
Dominique Makowski <dom.mak19@gmail.com>
2022-08-25 08:48:46 +0800

    move experiment inside experiment

--- preprocessing.R
+++ preprocessing.R
@@ -1,6 +1,6 @@
 preprocess_raw <- function(file) {
   file <- read.csv(file)
-  
+
   # Internal Utilities
   unnest <- function(scale){
     data <- file |>
@@ -9,7 +9,7 @@ preprocess_raw <- function(file) {
       jsontools::json_unnest_longer(col='response', indices_to = 'index')
     data
   }
-  
+
   # Preprocess
   if (!"end_screen" %in% file$screen) {
     print(paste0("Warning: Incomplete data for ", file))
@@ -17,19 +17,19 @@ preprocess_raw <- function(file) {
   }
   # Demographics
   dem <- file[file$screen == "demographics" & !is.na(file$screen), "response"]
-  
+
   # Trials
   trials<-file[file$screen=='stimuli', ]
   response<- file[file$screen=='questionnaire',]
-  
+
   # Questionnaire
   questionnaire<- file |>
     filter(screen == 'questionnaire') |>
     select(participant_id, response) |>
     jsontools::json_unnest_wider(col='response')
   questionnaire<- questionnaire|>rename(Participant='participant_id')
-    
-  # Info 
+
+  # Info
   info <- file[file$screen == "browser_info" & !is.na(file$screen), ]
   data <- data.frame(
     Participant = trials$participant_id,
@@ -56,8 +56,8 @@ preprocess_raw <- function(file) {
     Stimulus = gsub(".jgp", "", gsub("stimuli/AMFD/", "", trials$stimulus)),
     RT = as.numeric(response$rt)
   )
-  
-  # Psychometric Scales 
+
+  # Psychometric Scales
   # IPIP
   ipip6 <-unnest('IPIP6')|>
     pivot_wider(names_from='index', values_from='response')
@@ -68,24 +68,24 @@ preprocess_raw <- function(file) {
   data$Openness <- rowMeans(ipip6[grepl("Openness", names(ipip6))])
   data$HonestyHumility <- rowMeans(ipip6[grepl("HonestyHumility", names(ipip6))])
   data$Agreeableness <- rowMeans(ipip6[grepl("Agreeableness", names(ipip6))])
- 
+
   #SIAS
-  sias<-unnest('SIAS') 
+  sias<-unnest('SIAS')
   attention_check<- sias[sias$index=='Attention_Check_1',]
   attention_check<- attention_check |>
     pivot_wider(names_from='index', values_from='response')
   data$Attention_Check1<- attention_check$Attention_Check_1
-  
+
   sias <-sias|>
     filter(index!='Attention_Check_1')|>
     pivot_wider(names_from='index', values_from='response')
   data$SIAS_Mean <-rowMeans(sias[grepl("SIAS", names(sias))])
-  
+
   #SPS
   sps<-unnest('SPS')|>
     pivot_wider(names_from='index', values_from='response')
   data$SPS_Mean<- rowMeans(sps[grepl("Social_Phobia", names(sps))])
-             
+
   #GAAIS
   gaais<-unnest('GAAIS')|>
     pivot_wider(names_from='index', values_from='response')
@@ -93,12 +93,12 @@ preprocess_raw <- function(file) {
   data$GAAIS <-rowMeans(gaais[grepl("GAAIS", names(gaais))])
 
   #FFNI
-  ffni<-unnest('FFNI-BF') 
+  ffni<-unnest('FFNI-BF')
   attention_check<- ffni[ffni$index=='Attention_Check_2',]
   attention_check<- attention_check |>
     pivot_wider(names_from='index', values_from='response')
   data$Attention_Check2<- attention_check$Attention_Check_2
-  
+
   ffni<- ffni|>
     filter(index!='Attention_Check_2') |>
     pivot_wider(names_from='index', values_from='response')
@@ -123,39 +123,39 @@ preprocess_raw <- function(file) {
     pivot_wider(names_from=index, values_from=response)
   data$GPTS_Reference <-rowMeans(gpts[grepl("Reference", names(gpts))])
   data$GPTS_Persecution <-rowMeans(gpts[grepl("Persecution", names(gpts))])
-  
+
   #SCC
-  remove<- c('self_rated_general_attractiveness', 
-            'self_rated_physical_attractiveness', 
+  remove<- c('self_rated_general_attractiveness',
+            'self_rated_physical_attractiveness',
             'Attention_Check_3')
   scc<-unnest('SCC')
-  
+
   attention_check<- scc[scc$index=='Attention_Check_3',]
   attention_check<- attention_check |>
     pivot_wider(names_from='index', values_from='response')
   data$Attention_Check3<- attention_check$Attention_Check_3
-  
+
   self_attractiveness<-scc[scc$index=='self_rated_general_attractiveness',]
   self_attractiveness<- self_attractiveness|>
     pivot_wider(names_from = 'index', values_from='response')
   data$self_attractiveness <- self_attractiveness$self_rated_general_attractiveness
-  
+
   self_phy_attractiveness<-scc[scc$index=='self_rated_physical_attractiveness',]
   self_phy_attractiveness<- self_phy_attractiveness|>
     pivot_wider(names_from = 'index', values_from='response')
   data$self_phy_attractiveness <- self_phy_attractiveness$self_rated_physical_attractiveness
-  
+
   scc<-scc[!grepl(paste(remove, collapse='|'), scc$index),]|>
     pivot_wider(names_from='index', values_from='response')
   scc[grepl("_R", names(scc))] <- 100 - scc[grepl("_R", names(scc))]
   data$SCC <- rowMeans(scc[grepl("SCC", names(scc))])
-  
+
   #IUS
   ius<-unnest('IUS')|>
     pivot_wider(names_from=index, values_from=response)
   data$IUS_ProspectiveAnxiety <-rowMeans(ius[grepl("Prospective", names(ius))])
   data$IUS_InhibitoryAnxiety <-rowMeans(ius[grepl("Inhibitory", names(ius))])
-  
+
   data_total<- merge(data, questionnaire, by='Participant')
   data_total
 }
@@ -163,12 +163,12 @@ preprocess_raw <- function(file) {
 # This is a local folder containing raw data from unzipped pavlovia
 # It has been added to .gitignore to NOT be published on github
 # (it contains the subject ID of the participants)
-participants <- list.files("data/raw_data/")
+participants <- list.files("data/rawdata/")
 
 df <- data.frame()
 for (ppt in participants) {
-  df <- rbind(df, preprocess_raw(file = paste0("data/raw_data/", ppt)))
-  
+  df <- rbind(df, preprocess_raw(file = paste0("data/rawdata/", ppt)))
+
 }
 
 write.csv(df, "data/data.csv", row.names = FALSE)

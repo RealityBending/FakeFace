9ccf04cf7857bd14de27033045a947423683842b
An Shu Te <teanshu97@gmail.com>
2022-08-31 11:08:13 +0800

    update preprocessing

--- experiment/pilot/old_preprocessing.R
+++ experiment/pilot/old_preprocessing.R
@@ -7,26 +7,25 @@ unnest <- function(file, scale) {
 }
 
 preprocess_raw <- function(file) {
-  
+
   data <- read.csv(file)
-  
+
   # Preprocess
   if (!"end_screen" %in% data$screen) {
     print(paste0("Warning: Incomplete data for ", file))
     return(data.frame())
   }
-  
+
   # Demographics
   dem <- data[data$screen == "demographics" & !is.na(data$screen), "response"]
-  
+
   # Trials
   trials <- data[data$screen == "stimuli", ]
   answers <- unnest(data, "questionnaire")
-  realness_answers <- unnest(data, "perceived_realness")
-  
+
   # Info
   info <- data[data$screen == "browser_info" & !is.na(data$screen), ]
-  
+
   df <- data.frame(
     Participant = trials$participant_id,
     Age = as.numeric(jsonlite::fromJSON(dem[1])$age),
@@ -40,9 +39,8 @@ preprocess_raw <- function(file) {
     Ethnicity = tools::toTitleCase(jsonlite::fromJSON(dem[1])$ethnicity),
     Date = ifelse(is.null(info$date), NA, info$date),
     Time = ifelse(is.null(info$time), NA, info$time),
-    Duration_Questionnaires = sum(as.numeric(data[data$screen %in% c('IUS', 'IPIP6', 'SIAS', 'SPS', 'GAAIS', 'FFNI-BF', 'GPTS'), 'rt'])) / 1000 / 60,
-    Duration_Task = sum(as.numeric(data[data$screen =="questionnaire", "rt"])) /1000 /60,
-    Duration_Realness = sum(as.numeric(data[data$screen == "perceived_realness", "rt"])) / 1000 / 60,
+    Duration_Questionnaires = sum(as.numeric(data[data$screen %in% c('IUS', 'IPIP6', 'SIAS', 'SPS', 'GAAIS', 'FFNI-BF', 'GPTS', 'SCC'), 'rt'])) / 1000 / 60,
+    Duration_Task = sum(as.numeric(data[data$screen == "questionnaire", "rt"])) / 1000 / 60,
     Screen_Resolution = paste0(info$screen_width, "x", info$screen_height),
     Screen_Size = (as.numeric(info$screen_width) / 1000) * (as.numeric(info$screen_height) / 1000),
     Screen_Refresh = info$vsync_rate,
@@ -51,36 +49,34 @@ preprocess_raw <- function(file) {
     Device = ifelse(info$mobile == TRUE, "Mobile", "Desktop"),
     Device_OS = info$os,
     Trial = as.numeric(trials$trial_number),
-    Block = as.numeric(trials$block_number),
     Stimulus = gsub(".jgp", "", gsub("stimuli/AMFD/", "", trials$stimulus)),
-    Task_RT = as.numeric(data[data$screen == "questionnaire", "rt"]),
-    Realness_RT = as.numeric(data[data$screen =="perceived_realneess", "rt"]),
-    SimulationMonitoring = datawizard::change_scale(realness_answers$perceived_realness, to = c(-1, 1), range = c(0, 100)),
-   # ISI = as.numeric(data[data$screen == "fixation", "trial_duration"]),
-    Real = datawizard::change_scale(realness_answers$perceived_realness, to = c(0.0001, 0.9999), range = c(0, 100)),
+    RT = as.numeric(data[data$screen == "questionnaire", "rt"]),
+    SimulationMonitoring = datawizard::change_scale(answers$perceived_realness, to = c(-1, 1), range = c(0, 100)),
+    ISI = as.numeric(data[data$screen == "fixation", "trial_duration"]),
+    Real = datawizard::change_scale(answers$perceived_realness, to = c(0.0001, 0.9999), range = c(0, 100)),
     Attractive = datawizard::change_scale(answers$Attractiveness, to = c(0.0001, 0.9999), range = c(0, 100)),
-   # Approachable = datawizard::change_scale(answers$Approachability, to = c(0.0001, 0.9999), range = c(0, 100)),
+    Approachable = datawizard::change_scale(answers$Approachability, to = c(0.0001, 0.9999), range = c(0, 100)),
     Trustworthy = datawizard::change_scale(answers$Trustworthiness, to = c(0.0001, 0.9999), range = c(0, 100)),
-    Familiar = datawizard::change_scale(answers$Familiarity, to = c(0.0001, 0.9999), range = c(0, 100))
-  #  Similar = datawizard::change_scale(answers$Similarity, to = c(0.0001, 0.9999), range = c(0, 100))
+    Familiar = datawizard::change_scale(answers$Familiarity, to = c(0.0001, 0.9999), range = c(0, 100)),
+    Similar = datawizard::change_scale(answers$Similarity, to = c(0.0001, 0.9999), range = c(0, 100))
   )
-  
+
   # Format sexual orientation
   df$Sexual_Orientation <- ifelse(df$Sexual_Orientation == "Other", jsonlite::fromJSON(dem[4])$sexual_orientation, df$Sexual_Orientation)
-  
+
   # Format education
   df$Education <- gsub("University (", "", df$Education, fixed = TRUE)
   df$Education <- gsub(")", "", df$Education, fixed = TRUE)
   df$Education <- tools::toTitleCase(df$Education)
-  
+
   # Standardize demographics
   # unique(df$Ethnicity)
   df$Ethnicity <- ifelse(df$Ethnicity %in% c("Latin", "Hisapanic"), "Latino", df$Ethnicity)
-  
+
   # Add info related to stimulus
   df$Stimulus_Sex <- ifelse(str_detect(df$Stimulus, "NF"), "Female", "Male")
   df$Stimulus_SameSex <- ifelse(df$Sex == df$Stimulus_Sex, "Same", "Opposite")
-  
+
   # Psychometric Scales
   # IPIP
   ipip6 <- unnest(data, "IPIP6")
@@ -91,17 +87,17 @@ preprocess_raw <- function(file) {
   df$IPIP6_Openness <- rowMeans(ipip6[grepl("Openness", names(ipip6))])
   df$IPIP6_HonestyHumility <- rowMeans(ipip6[grepl("HonestyHumility", names(ipip6))])
   df$IPIP6_Agreeableness <- rowMeans(ipip6[grepl("Agreeableness", names(ipip6))])
-  
-  
+
+
   # SIAS + Attention Check 1
   sias <- unnest(data, "SIAS")
   df$Social_Anxiety <- rowMeans(sias[grepl("SIAS", names(sias))])
-  
+
   # SPS
   sps <- unnest(data, "SPS")
   df$Social_Phobia <- rowMeans(sps[grepl("Social_Phobia", names(sps))])
-  
-  
+
+
   # GAAIS
   gaais <- unnest(data, "GAAIS")
   gaais[grepl("_R", names(gaais))] <- 100 - gaais[grepl("_R", names(gaais))]
@@ -115,10 +111,10 @@ preprocess_raw <- function(file) {
   df$AI_8_Exciting <- gaais$GAAIS_8
   df$AI_9_Applications <- gaais$GAAIS_9
   df$AI_10_FaceErrors <- gaais$GAAIS_10
-  
-  
-  
-  
+
+
+
+
   # FFNI + Attention Check 2
   ffni <- unnest(data, "FFNI-BF")
   df$FFNI_AcclaimSeeking <- rowMeans(ffni[grepl("Acclaim", names(ffni))])
@@ -136,35 +132,36 @@ preprocess_raw <- function(file) {
   df$FFNI_ReactiveAnger <- rowMeans(ffni[grepl("Reactive", names(ffni))])
   df$FFNI_Shame <- rowMeans(ffni[grepl("Shame", names(ffni))])
   df$FFNI_ThrillSeeking <- rowMeans(ffni[grepl("Thrill", names(ffni))])
-  
-  df$SelfAttractiveness1 <- ffni$self_rated_general_attractiveness
-  df$SelfAttractiveness2 <- ffni$self_rated_physical_attractiveness
-  
+
+
+
   # GPTS
   gpts <- unnest(data, "GPTS")
   df$GPTS_Reference <- rowMeans(gpts[grepl("Reference", names(gpts))])
   df$GPTS_Persecution <- rowMeans(gpts[grepl("Persecution", names(gpts))])
-  
-  
-  # SCC + Attention Check 3
-  # scc <- unnest(data, "SCC")
-  # scc[grepl("_R", names(scc))] <- 100 - scc[grepl("_R", names(scc))]
-  # df$SelfConceptClarity <- rowMeans(scc[grepl("SCC", names(scc))])
-  
 
-  
+
+  # SCC + Attention Check 3
+  scc <- unnest(data, "SCC")
+  scc[grepl("_R", names(scc))] <- 100 - scc[grepl("_R", names(scc))]
+  df$SelfConceptClarity <- rowMeans(scc[grepl("SCC", names(scc))])
+
+  df$SelfAttractiveness1 <- scc$self_rated_general_attractiveness
+  df$SelfAttractiveness2 <- scc$self_rated_physical_attractiveness
+
+
   # IUS
   ius <- unnest(data, "IUS")
   df$IUS_ProspectiveAnxiety <- rowMeans(ius[grepl("Prospective", names(ius))])
   df$IUS_InhibitoryAnxiety <- rowMeans(ius[grepl("Inhibitory", names(ius))])
-  
-  
+
+
   # Attention checks
   df$Attention_Check1 <- sias$Attention_Check_1
   df$Attention_Check2 <- ffni$Attention_Check_2
-  df$Attention_Check3 <- 100 - ius$Attention_Check_3  # Reversed
-  
-  
+  df$Attention_Check3 <- 100 - scc$Attention_Check_3  # Reversed
+
+
   # Combine with Norms data
   norms <- read.csv("experiment/stimuli/AMFD_norms.csv")
   norms <- norms[norms$FType == 0, ]  # No smiling
@@ -176,7 +173,7 @@ preprocess_raw <- function(file) {
     Norms_Warm = norms$Warm_mean,
     Norms_Attractive = norms$Attract_mean
   )
-  
+
   merge(df, norms, by = "Stimulus")
 }
 
@@ -191,4 +188,4 @@ for (ppt in participants) {
 }
 
 
-write.csv(df, "data/data2.csv", row.names = FALSE)
+write.csv(df, "data/data.csv", row.names = FALSE)
